7fd6e5583db02462b90f7ab9ec6ac62b9d10905c
Andreas Kahler <mail@andreaskahler.com>
2023-03-06 22:03:39 +0100

    generate list of 3dprinted parts for gitbuilding

--- src/mech/cadquery_workflow/generate.py
+++ src/mech/cadquery_workflow/generate.py
@@ -9,6 +9,7 @@ import models.nimble_beam
 
 outputdir_stl = "./src/mech/cadquery_workflow/gitbuilding/models/"
 outputdir_step = "./src/mech/step/"
+outputdir_gitbuilding = "./src/mech/cadquery_workflow/gitbuilding/"
 
 beam_length = 294
 
@@ -29,10 +30,26 @@ except:
 beam = models.nimble_beam.create(beam_length=beam_length)
 
 # export 
-cq.exporters.export(beam, outputdir_stl + "beam.stl")
-cq.exporters.export(beam, outputdir_step + "beam.step")
+
+partList = []
+
+def exportPart(part, name):    
+  cq.exporters.export(beam, outputdir_stl + name + ".stl")
+  cq.exporters.export(beam, outputdir_step + name + ".step")
+  partList.append(name)
 
 
+exportPart(beam, "beam")
+exportPart(beam, "test")
+
+# write gitbuilding files
+
+with open(outputdir_gitbuilding+'3dprintingparts.md', 'w') as f:
+    f.write("# 3D print all the needed files\n\n")
+    for part in partList:
+      f.write("* %s.stl ([preview](models/%s.stl){previewpage}, [download](models/%s.stl))\n" % (part, part, part))
+
+#for debugging
 #show_object(beam)
 
 
