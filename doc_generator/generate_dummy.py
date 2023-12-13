b5e1bafff7ac64da5ae7972dbd65f522ed1b9392
Jeremy Wright <wrightjmf@gmail.com>
2023-12-13 12:56:54 -0500

    Forgot to pass the config to the orchestration script as well

--- doc_generator/generate_dummy.py
+++ doc_generator/generate_dummy.py
@@ -1,5 +1,5 @@
 # import models.cadquery.rack_leg
 
-def generate_docs(config_hash):
+def generate_docs(config, config_hash):
     print("I am modifying config files and running ExSource-Tools...")
     print("I will put my finished artifacts in a directory marked with the config hash: " + str(config_hash))
