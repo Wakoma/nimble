791da1e2ba7c7905b309d9f430f046b28147e335
Andreas Kahler <mail@andreaskahler.com>
2024-02-10 13:38:43 +0100

    allow components to be created when accessing them as python modules

--- doc_generator/mechanical/components/cadquery/top_plate.py
+++ doc_generator/mechanical/components/cadquery/top_plate.py
@@ -3,7 +3,10 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 import cadscript as cad
-from nimble_builder.nimble_end_plate import create_end_plate
+if __name__ == "__cqgi__":
+    from nimble_builder.nimble_end_plate import create_end_plate
+else:
+    from .nimble_builder.nimble_end_plate import create_end_plate
 
 # parameters to be set in exsource-def.yaml file
 width = 100
@@ -16,5 +19,6 @@ def create(width, depth, height):
     return create_end_plate(width, depth, height)
 
 
-result = create(width, depth, height)
-cad.show(result)  # when run in cq-cli, will return result
+if __name__ == "__cqgi__":
+    result = create(width, depth, height)
+    cad.show(result)  # when run in cq-cli, will return result
