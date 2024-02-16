899292da5ba17491946a20d937df48ececaef567
Jeremy Wright <wrightjmf@gmail.com>
2024-02-16 16:11:04 -0500

    Changed server to use CQGI per #15

--- doc_generator/server/nimble_server.py
+++ doc_generator/server/nimble_server.py
@@ -88,6 +88,29 @@ def check_model_format(model_format):
         return True
 
 
+def cqgi_model_script(file_name, params):
+    """
+    Handle executing the model script via CQGI.
+    """
+
+    from cadquery import cqgi
+
+    # Read and execute the rack leg script
+    cq_path = os.path.join("..", "mechanical", "components", "cadquery", file_name)
+    user_script = ""
+    with open(cq_path) as f:
+        user_script = f.read()
+
+    # Build the object with the customized parameters and get it ready to export
+    build_result = cqgi.parse(user_script).build(build_parameters=params)
+    if build_result.success:
+        res = build_result.results[0].shape
+    else:
+        return HTMLResponse(f'ERROR: There was an error executing the script: {build_result.exception}')
+
+    return res
+
+
 # http://127.0.0.1:8000/wakoma/nimble/rack_leg?length=50.0&model_format=stl
 @app.get("/wakoma/nimble/rack_leg")
 def read_item(length: float = 294.0, hole_spacing: float = 14.0, long_axis_hole_dia: float = 4.6, mounting_holes_dia:float = 3.6, model_format: str = "stl"):
@@ -96,37 +119,149 @@ def read_item(length: float = 294.0, hole_spacing: float = 14.0, long_axis_hole_
     """
     # Check to make sure the user requested a valid model format
     if not check_model_format(model_format):
-        return HTMLResponse('ERROR: Model format not supported. <a href="https://7bindustries.com/hardware/rack_leg_download.html">Try Again</a>')
+        return HTMLResponse(f'ERROR: Model format {model_format} is not supported. Please go back and try again.')
 
     # Check to make sure the user has not set a length that is too long
     if length > 1000:
-        return HTMLResponse('ERROR: Length parameter is too large. <a href="https://7bindustries.com/hardware/rack_leg_download.html">Try Again</a>')
+        return HTMLResponse('ERROR: Length parameter is too large. Please check your parameters and try again.')
 
     # Make sure that the hole spacing is valid
     if hole_spacing <= 0.0 or hole_spacing >= length or hole_spacing <= mounting_holes_dia:
-         return HTMLResponse('ERROR: Invalid hole_spacing parameter. <a href="https://7bindustries.com/hardware/rack_leg_download.html">Try Again</a>')
+         return HTMLResponse('ERROR: Invalid hole_spacing parameter. Please check your parameters and try again.')
 
     # Make sure that the long axis hole diameter is valid
     if long_axis_hole_dia <= 0.0 or long_axis_hole_dia >= 20.0:
-        return HTMLResponse('ERROR: Invalid long_axis_hole_dia parameter. <a href="https://7bindustries.com/hardware/rack_leg_download.html">Try Again</a>')
+        return HTMLResponse('ERROR: Invalid long_axis_hole_dia parameter. Please check your parameters and try again.')
 
     # Make sure that the mounting hole diameter is valid
     if mounting_holes_dia <= 0.0 or mounting_holes_dia >= 20.0:
-        return HTMLResponse('ERROR: Invalid mounting_holes_dia parameter. <a href="https://7bindustries.com/hardware/rack_leg_download.html">Try Again</a>')
+        return HTMLResponse('ERROR: Invalid mounting_holes_dia parameter. Please check your parameters and try again.')
+
+    # Add the CadQuery model path so that all models only need to do absolute imports
+    cq_path = os.path.join("..", "mechanical", "components", "cadquery")
+    sys.path.append(cq_path)
 
     import cadquery as cq
-    from mechanical.components.cadquery.rack_leg import make_rack_leg
 
-    # Generate the rack leg
-    leg = make_rack_leg(length, hole_spacing, long_axis_hole_dia, mounting_holes_dia).cq()
+    # Run the script with customized parameters
+    leg = cqgi_model_script("rack_leg.py", {'length': length, 'hole_spacing': hole_spacing, 'long_axis_hole_dia': long_axis_hole_dia, 'mounting_holes_dia': mounting_holes_dia})
+
+    # In case there was an error
+    if (type(leg).__name__ == "HTMLResponse"):
+        return leg
 
     # Figure out what the file name and temporary path should be
-    export_file_name = "leg_length-" + str(length) + "_hole_spacing-" + str(hole_spacing) + "_long_axis_hole_dia-" + str(long_axis_hole_dia) + "_mounting_holes_dia-" + str(mounting_holes_dia) + "." + model_format
+    export_file_name = "rack_leg-leg_length-" + str(length) + "_hole_spacing-" + str(hole_spacing) + "_long_axis_hole_dia-" + str(long_axis_hole_dia) + "_mounting_holes_dia-" + str(mounting_holes_dia) + "." + model_format
     export_path = os.path.join(tempfile.gettempdir(), export_file_name)
 
     # If the leg does not already exist, export it to a temporary file
     if not os.path.exists(export_path):
         cq.exporters.export(leg, export_path)
+
+    return FileResponse(export_path, headers={'Content-Disposition': 'attachment; filename=' + export_file_name})
+
+
+# http://127.0.0.1:8000/wakoma/nimble/top_plate?width=100&depth=100&height=3&model_format=stl
+@app.get("/wakoma/nimble/top_plate")
+def read_item(width: float = 100, depth: float = 100, height: float = 3, model_format: str = "stl"):
+    # Check to make sure the user requested a valid model format
+    if not check_model_format(model_format):
+        return HTMLResponse(f'ERROR: Model format {model_format} is not supported. Please check your parameters and try again.')
+
+    # Check to make sure all the model dimensions are positive
+    if width < 0 or depth < 0 or height < 0:
+        return HTMLResponse(f'ERROR: None of the model dimensions can be negative. Please check your parameters and try again.')
+
+    # Add the CadQuery model path so that all models only need to do absolute imports
+    cq_path = os.path.join("..", "mechanical", "components", "cadquery")
+    sys.path.append(cq_path)
+
+    import cadquery as cq
+
+    # Run the script with customized parameters
+    plate = cqgi_model_script("top_plate.py", {'width': width, 'depth': depth, 'height': height})
+
+    # In case there was an error
+    if (type(plate).__name__ == "HTMLResponse"):
+        return plate
+
+    # Figure out what the file name and temporary path should be
+    export_file_name = "top_plate_width-" + str(width) + "_depth-" + str(depth) + "_height-" + str(height) + "." + model_format
+    export_path = os.path.join(tempfile.gettempdir(), export_file_name)
+
+    # If the leg does not already exist, export it to a temporary file
+    if not os.path.exists(export_path):
+        cq.exporters.export(plate, export_path)
+
+    return FileResponse(export_path, headers={'Content-Disposition': 'attachment; filename=' + export_file_name})
+
+
+# http://127.0.0.1:8000/wakoma/nimble/tray?number_of_units=2&tray_width=115&tray_depth=115&model_format=stl
+@app.get("/wakoma/nimble/tray")
+def read_item(number_of_units: float = 2, tray_width: float = 115, tray_depth: float = 115, model_format: str = "stl"):
+    # Check to make sure the user requested a valid model format
+    if not check_model_format(model_format):
+        return HTMLResponse(f'ERROR: Model format {model_format} is not supported. Please check your parameters and try again.')
+
+    # Check to make sure all the model dimensions are positive
+    if number_of_units < 0 or tray_width < 0 or tray_depth < 0:
+        return HTMLResponse(f'ERROR: None of the model dimensions can be negative. Please check your parameters and try again.')
+
+    # Add the CadQuery model path so that all models only need to do absolute imports
+    cq_path = os.path.join("..", "mechanical", "components", "cadquery")
+    sys.path.append(cq_path)
+
+    import cadquery as cq
+
+    # Run the script with customized parameters
+    tray = cqgi_model_script("nimble_tray.py", {"height_in_hole_unites": number_of_units, "tray_width": tray_width, "tray_depth": tray_depth})
+
+    # In case there was an error
+    if (type(tray).__name__ == "HTMLResponse"):
+        return tray
+
+    # Figure out what the file name and temporary path should be
+    export_file_name = "tray_number_of_units-" + str(number_of_units) + "_tray_width-" + str(tray_width) + "_tray_depth-" + str(tray_depth) + "." + model_format
+    export_path = os.path.join(tempfile.gettempdir(), export_file_name)
+
+    # If the leg does not already exist, export it to a temporary file
+    if not os.path.exists(export_path):
+        cq.exporters.export(tray, export_path)
+
+    return FileResponse(export_path, headers={'Content-Disposition': 'attachment; filename=' + export_file_name})
+
+
+# http://127.0.0.1:8000/wakoma/nimble/base_plate?width=100&depth=100&height=3&model_format=stl
+@app.get("/wakoma/nimble/base_plate")
+def read_item(width: float = 100, depth: float = 100, height: float = 3, model_format: str = "stl"):
+    # Check to make sure the user requested a valid model format
+    if not check_model_format(model_format):
+        return HTMLResponse(f'ERROR: Model format {model_format} is not supported. Please check your parameters and try again.')
+
+    # Check to make sure all the model dimensions are positive
+    if width < 0 or depth < 0 or height < 0:
+        return HTMLResponse(f'ERROR: None of the model dimensions can be negative. Please check your parameters and try again.')
+
+    # Add the CadQuery model path so that all models only need to do absolute imports
+    cq_path = os.path.join("..", "mechanical", "components", "cadquery")
+    sys.path.append(cq_path)
+
+    import cadquery as cq
+
+    # Run the script with customized parameters
+    base_plate = cqgi_model_script("base_plate.py", {"width": width, "depth": depth, "height": height})
+
+    # In case there was an error
+    if (type(base_plate).__name__ == "HTMLResponse"):
+        return base_plate
+
+    # Figure out what the file name and temporary path should be
+    export_file_name = "base_plate_width-" + str(width) + "_depth-" + str(depth) + "_height-" + str(height) + "." + model_format
+    export_path = os.path.join(tempfile.gettempdir(), export_file_name)
+
+    # If the leg does not already exist, export it to a temporary file
+    if not os.path.exists(export_path):
+        cq.exporters.export(base_plate, export_path)
 
     return FileResponse(export_path, headers={'Content-Disposition': 'attachment; filename=' + export_file_name})
 
