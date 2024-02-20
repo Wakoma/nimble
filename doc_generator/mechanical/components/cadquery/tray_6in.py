053d9a12d8a51fe83f3512976b334c583e3db23f
Andreas Kahler <mail@andreaskahler.com>
2024-02-20 22:51:01 +0100

    usw-flex shelf

--- doc_generator/mechanical/components/cadquery/tray_6in.py
+++ doc_generator/mechanical/components/cadquery/tray_6in.py
@@ -11,8 +11,9 @@ from nimble_builder import shelf_builder
 # "stuff"       - for general stuff
 # "stuff-thin"  - for general stuff, thin version
 # "nuc"         - for Intel NUC
-shelf_type = "nuc"
-hole_count = 4
+# "usw-flex"    - for Ubiquiti USW-Flex
+shelf_type = "usw-flex"
+hole_count = 3
 
 
 def get_builder(hole_count) -> shelf_builder.ShelfBuilder:
@@ -39,6 +40,20 @@ def create_6in_shelf(shelf_type, hole_count) -> cad.Body:
         b.add_mounting_hole_to_bottom(x_pos=0, y_pos=35, base_thickness=4, hole_type="M3cs")
         b.add_mounting_hole_to_bottom(x_pos=0, y_pos=120, base_thickness=4, hole_type="M3cs")
         return b.get_body()
+    if shelf_type == "usw-flex":
+        b = get_builder(hole_count)
+        b.make_front(front_type="full", bottom_type="closed")
+        b.cut_opening("<Y", b._inner_width, 4)
+        b.make_tray(width="standard", depth=119.5, sides="w-pattern", back="open")
+        # add 2 mounting bars on the bottom plate
+        sketch = cad.make_sketch()
+        sketch.add_rect(8, 60, center="X", pos=[(-17.5, 42), (+17.5, 42)])
+        base = cad.make_extrude("XY", sketch, shelf_builder.bottom_thickness)
+        sketch.cut_circle(d=3.8, pos=[(-17.5, 30 + 42), (+17.5, 30 + 42)])
+        base2 = cad.make_extrude("XY", sketch, 5)
+        b.get_body().add(base).add(base2)
+        return b.get_body()
+
     raise ValueError(f"Unknown shelf type: {shelf_type}")
 
 
