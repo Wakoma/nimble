e9562c286642b6632a19dd1354726753d22efadb
Jeremy Wright <wrightjmf@gmail.com>
2024-05-10 12:14:27 -0400

    First pass at updating devices.json with associated fixes

--- nimble_orchestration/device.py
+++ nimble_orchestration/device.py
@@ -5,22 +5,48 @@ class Device:
 
     Example node:
       {
-        "ID": "NetgateSG1100",
-        "Name": "Netgate SG-1100",
-        "Category": "Firewall",
-        "HeightInUnits": 2,
-        "Width": "Standard",
-        "TrayType": "Open"
-      },
+        "ID": "mantbox_2_12smantbox_2_12s",
+        "Type": "Access Point",
+        "Hardware": "mANTBox 2 12s",
+        "Brand": "MikroTik",
+        "Model": "mANTBox 2 12s",
+        "Shelf": "No shelf needed",
+        "Rack": "None",
+        "ShelfId": "",
+        "HeightUnits": "",
+        "LengthMm": "140.00 mm",
+        "Depth": "348.00 mm",
+        "Height": "82.00 mm",
+        "PoEIn": "Passive PoE",
+        "PoEOut": "",
+        "WattageMax": "11.0 W",
+        "EstCostUsd": "$125.00",
+        "ProductWebsiteUsd": "https://mikrotik.com/product/mantbox_2_12s",
+        "ProductWebsiteEur": "",
+        "RAM": "",
+        "Field19": "",
+        "PoEInInputVoltage": "10-28 V",
+        "Voltage": "",
+        "Amperage": "",
+        "WRTVersion": "",
+        "OpenWRTLink": "",
+        "Comments": ""
+      }
     """
 
     def __init__(self, jsonNode):
         self.id = jsonNode['ID']
-        self.name = jsonNode['Name']
-        self.category = jsonNode['Category']
-        self.height_in_units = jsonNode['HeightInUnits']
-        self.width = jsonNode['Width']
-        self.tray_type = jsonNode['TrayType']
+        # self.name = jsonNode['Name']
+        self.name = jsonNode['Hardware']
+        # self.category = jsonNode['Category']
+        self.category = jsonNode['Type']
+        self.height_in_units = int(jsonNode['HeightUnits'])
+        # self.width = jsonNode['Width']
+        self.width = jsonNode['LengthMm']
+        self.depth = jsonNode['Depth']
+        self.shelf_id = jsonNode['ShelfId']
+        # self.tray_type = jsonNode['TrayType']
+        self.shelf_type = jsonNode['Shelf']
 
     def get_tray_id(self):
-        return f"tray_h{self.height_in_units}_t{self.tray_type}"
+        return f"tray_h{self.height_in_units}_t{self.shelf_type.lower().replace(' ', '_')}"
