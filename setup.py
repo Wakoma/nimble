dbc82aef19a7e4567344dacf470112864a4e7aaf
Jeremy Wright <wrightjmf@gmail.com>
2024-05-29 21:29:23 -0400

    Started on an automated assembly step generator

--- setup.py
+++ setup.py
@@ -13,7 +13,9 @@ setup(
         'cadscript>=0.5.2',
         'exsource-tools',
         'cq-cli @ git+https://github.com/CadQuery/cq-cli.git',
-        'gitbuilding==0.15.0a2'
+        'gitbuilding==0.15.0a2',
+        'cq-annotate @ git+https://github.com/jmwright/cq-annotate.git',
+        'cq_warehouse @ git+https://github.com/gumyr/cq_warehouse.git',
     ],
     extras_require={
         'dev': [
