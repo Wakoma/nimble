bbce13a25426603460ddac5c13e5863246aea758
Jeremy Wright <wrightjmf@gmail.com>
2024-08-22 21:05:10 -0400

    Trying to see if forcing the PATH variable helps fix CI

--- nimble_build_system/orchestration/paths.py
+++ nimble_build_system/orchestration/paths.py
@@ -3,9 +3,7 @@ This module to defines the paths used by orchestration scripts
 """
 
 import os
-from pathlib import Path
 
-# MODULE_PATH = os.path.join(os.path.dirname(__file__), '..', '..')
-MODULE_PATH = Path(__file__).parent.parent.parent
+MODULE_PATH = os.path.join(os.path.dirname(__file__), '..', '..')
 BUILD_DIR = os.path.join(MODULE_PATH, "build")
 REL_MECH_DIR = os.path.relpath(os.path.join(MODULE_PATH, "mechanical"), BUILD_DIR)
