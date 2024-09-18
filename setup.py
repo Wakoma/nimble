10e73ac4a604c8c8f56b671ec7882c2d646d6326
Jeremy Wright <wrightjmf@gmail.com>
2024-09-18 09:24:20 -0400

    Trying to pin the cadquery-ocp version to prevent segfault

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     install_requires=[
         'numpy~=1.26',
         # 'cadquery>=2',
+        'cadquery-ocp<=7.7.2.0'
         'cadquery @ git+https://github.com/CadQuery/cadquery.git',
         'cadquery-png-plugin @ git+https://github.com/jmwright/cadquery-png-plugin.git',
         'cadscript>=0.5.2',
