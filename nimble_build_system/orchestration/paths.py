7856512558084870d8eaee54760ae3ffb9818bf8
kny5 <antonio.anaya@kny5.work>
2025-09-03 01:48:29 +0100

    fixing path variable and dockerfile testing server.

--- nimble_build_system/orchestration/paths.py
+++ nimble_build_system/orchestration/paths.py
@@ -9,7 +9,7 @@ import logging
 ABS_PATH = os.path.abspath(os.getcwd())
 MODULE_PATH = os.path.join(os.path.dirname(__file__), '..', '..')
 REL_MECH_DIR = os.path.join(ABS_PATH, "mechanical")
-BUILD_DIR = os.path.join(MODULE_PATH, "build")
+BUILD_DIR = os.path.join(ABS_PATH, "build")
 
 
 logging.info(f"{ABS_PATH}\n\t\t\t\t\t{MODULE_PATH}\n\t\t\t\t\t{REL_MECH_DIR}")
